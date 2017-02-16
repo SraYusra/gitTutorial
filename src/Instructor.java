66f18274b65ae49e16a2040d9e2a751ea69d64b0
SraYusra <yusra.mohd@hotmail.com>
2017-02-15 22:57:44 -0500

    Added fulltime instance variable to Student.

--- src/Instructor.java
+++ src/Instructor.java
@@ -7,6 +7,7 @@ public class Instructor extends Person {
     public Instructor(String name, String emailAddress, String title){
         super(name,emailAddress);
         this.title=title;
+        this.favCourse=favCourse;
     }
 
     public String toString(){
